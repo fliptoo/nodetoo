59ea1cbd90367b9f9a8986ffb80f1d6c292475bc
javatar <javatar@hanyi.local>
2013-02-25 10:24:44 +0800

    add user.js

---
+++ examples/basic/app/models/user.js
@@ -0,0 +1,27 @@
+
+/**
+ * User Database
+ * @type {Array}
+ */
+var users = [
+  {
+      username: 'user'
+    , password: 'password'
+    , roles:    ['user']
+  },
+  {
+      username: 'admin'
+    , password: 'password'
+    , roles:    ['admin']
+  },
+  {
+      username: 'root'
+    , password: 'password'
+    , roles:    ['root']
+  },
+  {
+      username: 'mix'
+    , password: 'password'
+    , roles:    ['admin', 'root']
+  }
+]
