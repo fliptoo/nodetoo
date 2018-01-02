75d81a74c2c8d5082b793b4e0c57193de34cecd9
Huang Yee <fliptoo.studio@gmail.com>
2018-01-02 12:44:21 +0800

    Update o.js

--- o.js
+++ o.js
@@ -1,1 +1,3 @@
-alert('hello');
+! function() {
+    "undefined" == typeof e && (e = "Download Digital File"), Shopify.Checkout.OrderStatus.addContentBox("<div id='downloads'>        <a href='http://" + Shopify.shop + "/apps/downloads/orders/view/" + encodeURIComponent(Shopify.checkout.email) + "/" + Shopify.checkout.order_id + "' class='btn'>          " + e + "        </a>      </div>")
+}();
